d714ed635e24f7f16e884ad3c69bc871340238bd
prabakaranNexTurn <prabakaran.p@nexturn.com>
2026-03-16 11:33:37 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
-AWS_SECRET_ACCESS_KEY = "REDACTED_SECRET"
+AWS_SECRET_ACCESS_KEY = "AKIAIOSFODNN7EXAMPLE"
 REDACTED_SECRET
 db_connection = "postgres://user:pass@db.example.com"
 api_key = "sk-9asd89as7d9as7d"
